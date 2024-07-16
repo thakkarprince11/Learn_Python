d8c706cce43faeb354c8396a78ff373a60257a23
thakkarprince11 <124695240+thakkarprince11@users.noreply.github.com>
2024-07-16 19:27:03 +0530

    first commit

---
+++ 01_Basics/first.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+# Function is def in python
+
+def funprint(num):
+    print(num)
+
+funprint(5)         #Calling def
+funprint("Earth")   #Calling def
+
+
+car_one = "Porsche Taycan Turbo S"
+car_two = "Porsche GT3 RS"
+car_three = "Porsche GT2"
---
+++ 01_Basics/second.py
@@ -0,0 +1,3 @@
+from first import funprint
+
+funprint("Method is defined in first file and importing here and using to print")
